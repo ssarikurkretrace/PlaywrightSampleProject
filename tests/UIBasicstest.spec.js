f39d957eff81b9970f2cd098b6f2a2de07a8678e
Suleyman Sarikurk <suleymansarikurk@gmail.com>
2025-07-01 10:37:15 +0300

    login test finished

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,63 +1,70 @@
-const {test, expect, selectors} = require('@playwright/test');
-require('dotenv').config(); 
+const { test, expect, selectors } = require('@playwright/test');
+const { POManager } = require('../pageobjects/POManager');
+require('dotenv').config();
 
-test('Browser Context Playwright Test', async ({browser}) =>
-{
-const context = await browser.newContext();
-const page = await context.newPage();
-await page.goto (process.env.URL);
-await page.waitForTimeout(50000);
+test('Browser Context Playwright Test', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto(process.env.URL);
+    await page.waitForTimeout(50000);
 });
 
 
-test('Page Playwright Test', async ({page}) => {
-await page.goto (process.env.URL);
-console.log(await page.title)
-await expect(page).toHaveTitle("Login")
-// await page.waitForTimeout(50000);
+test('Page Playwright Test', async ({ page }) => {
+    await page.goto(process.env.URL);
+    console.log(await page.title)
+    await expect(page).toHaveTitle("Login")
+    // await page.waitForTimeout(50000);
 
-const partnerName = "qa-cls5"
-const practiceName = "CLS2"
-// const practiceName = "Headquarters"
+    const partnerName = "qa-cls5"
+    const practiceName = "CLS2"
+    // const practiceName = "Headquarters"
 
-await page.getByRole("textbox", {name: "Enter your email address"}).fill(process.env.SUPERUSER)
-await page.getByPlaceholder("Enter your password").fill(process.env.SUPERPASS)
-await page.getByPlaceholder("Enter the Partner ID").fill(partnerName)
-// await page.getByRole(" ")
+    await page.getByRole("textbox", { name: "Enter your email address" }).fill(process.env.SUPERUSER)
+    await page.getByPlaceholder("Enter your password").fill(process.env.SUPERPASS)
+    await page.getByPlaceholder("Enter the Partner ID").fill(partnerName)
+    // await page.getByRole(" ")
 
-await page.getByRole( "button", {name:'Login'} ).click()
-await page.waitForTimeout(3000);
+    await page.getByRole("button", { name: 'Login' }).click()
+    await page.waitForTimeout(3000);
 
-// check practice name is correct 
-const practiceExisting = await page.locator("._practice_name_13kkp_123").textContent();
-console.log("existing practice = " + practiceExisting)
-console.log("practice name= " + practiceName)
-if(practiceExisting != practiceName){
-    await page.locator("._practice_name_13kkp_123").click();
-    const practiceNameList = await page.locator(".ant-collapse-content-active ._practice_name_13kkp_123").allTextContents();
-    console.log(practiceNameList[0])
-    console.log("result = "+practiceNameList.length);
-    console.log("result = "+practiceNameList);
+    // check practice name is correct 
+    const practiceExisting = await page.locator("._practice_name_13kkp_123").textContent();
+    console.log("existing practice = " + practiceExisting)
+    console.log("practice name= " + practiceName)
+    if (practiceExisting != practiceName) {
+        await page.locator("._practice_name_13kkp_123").click();
+        const practiceNameList = await page.locator(".ant-collapse-content-active ._practice_name_13kkp_123").allTextContents();
+        console.log(practiceNameList[0])
+        console.log("result = " + practiceNameList.length);
+        console.log("result = " + practiceNameList);
 
-    await page.locator("._practice_name_13kkp_123:has-text('"+practiceName+"')").click();
-}
+        await page.locator("._practice_name_13kkp_123:has-text('" + practiceName + "')").click();
+    }
 
-await page.locator("._navbarItem_13kkp_199").getByText("Patients").click();
+    await page.locator("._navbarItem_13kkp_199").getByText("Patients").click();
 
-await page.getByPlaceholder("Search Name ID").fill("Ave")
-await page.locator("[name=' Carla Ave']").click()
+    await page.getByPlaceholder("Search Name ID").fill("Ave")
+    await page.locator("[name=' Carla Ave']").click()
 
 
 
-await page.pause();
+    await page.pause();
 
-// await page.pause();
-// select correct practice
+    // await page.pause();
+    // select correct practice
 });
 
-test('Saucelab Simple Login Test', async ({page}) => {
-await page.goto (process.env.URLSauce);
+test('Saucelab Simple Login Test', async ({ page }) => {
+    const poManager = new POManager(page);
+    const loginPage = poManager.getLoginPage();
+    await page.goto(process.env.URLSauce);
+    //enter username and password
+    await loginPage.usernameSaucelab.fill(process.env.sauceLabsStandardUser);
+    await loginPage.passwordSaucelab.fill(process.env.sauceLabsPass);
 
+    // verify if I am in the login page
+    await expect(page).toHaveTitle("Swag Labs");
 
 
 });
