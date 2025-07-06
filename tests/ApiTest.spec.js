f1cb953c159ef9185ba625c2d531b09dcef36e98
Suleyman Sarikurk <suleymansarikurk@gmail.com>
2025-07-06 12:42:07 +0300

    document formated

--- tests/ApiTest.spec.js
+++ tests/ApiTest.spec.js
@@ -1,32 +1,32 @@
-const {test, expect, request} = require('@playwright/test');
-const {APIUtils} = require('./utils/APIUtils');
+const { test, expect, request } = require('@playwright/test');
+const { APIUtils } = require('./utils/APIUtils');
 const loginPayLoad = {
     user: process.env.SUPERUSER,
     password: process.env.SUPERPASS,
     expiresIn: 100,
     partnerId: process.env.TESTPARTNER
-    };
+};
 
 let response;
 let token;
- test('Generate Token', async ()=>{
+test('Generate Token', async () => {
 
     const apiContext = await request.newContext({
         ignoreHTTPSErrors: true // ← this line fixes the SSL error
     });
 
-    const apiUtils = new APIUtils(apiContext,loginPayLoad);
+    const apiUtils = new APIUtils(apiContext, loginPayLoad);
     // token =  await apiUtils.generateToken(loginPayLoad);
     // console.log(token);
-    const patientData = await apiUtils.getPatientData(process.env.TESTPARTNER,process.env.TESTPRACTICE,"1707");
+    const patientData = await apiUtils.getPatientData(process.env.TESTPARTNER, process.env.TESTPRACTICE, "1707");
 
     console.log(patientData);
 
     const firstName = patientData.first_name;
-    console.log("Patient First Name = "+firstName);
+    console.log("Patient First Name = " + firstName);
 
 
- });
+});
 
 
 
