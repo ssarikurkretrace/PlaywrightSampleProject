8faf45d6828a29404376ef9c7c32023645461915
Suleyman Sarikurk <suleymansarikurk@gmail.com>
2025-07-09 10:42:57 +0300

    sqlite.js connection create added

--- tests/utils/SqliteUtils.js
+++ tests/utils/SqliteUtils.js
@@ -1,10 +1,29 @@
 const sqlite3 = require('sqlite3').verbose();
+const dbUrl = {
+    filename: process.env.SQL_LITE,
+    mode: sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE
+};
 
 class SqliteUtils {
 
-  constructor() {
-    this.connection = null;
-  }
-  
+    constructor() {
+        this.connection = null;
+    }
+    /**
+     * Creates a connection to a SQLite database.
+     */
+    async createConnection() {
+        return new Promise((resolve, reject) => {
+            this.connection = new sqlite3.Database(dbUrl.filename, dbUrl.mode, (err) => {
+                if (err) {
+                    console.error(`❌ Connection failed to SQLite DB: ${err.message}`);
+                    reject(err);
+                } else {
+                    console.log(`✅ Connected to SQLite DB: ${dbUrl.filename}`);
+                    resolve(this.connection);
+                }
+            });
+        });
+    }
 
-} 
+}
